034947a414161f7f4a50f975d6ff3952ed986c58
midroid <ch.m.aakash@gmail.com>
2017-12-04 02:32:51 +0900

    RNN completed

--- my_answers.py
+++ my_answers.py
@@ -12,23 +12,47 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
-
+    index = 0
+    while index + window_size < len(series):
+        X.append(series[index:index+window_size])
+        y.append(series[index+window_size])
+        index += 1
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
-
+    
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+    model.summary()
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
-    punctuation = ['!', ',', '.', ':', ';', '?']
+    # punctuation = ['!', ',', '.', ':', ';', '?']
+    import re
+    import string
+
+    ASCII_LETTERS = string.ascii_letters
+    ENGLISH_PUNCTUATION = ',.\'!?;:'
+
+    # find all unique characters in the text
+    uniques = ''.join(set(text))
+
+    # remove as many non-english characters and character sequences as you can 
+    for char in uniques:
+        if char not in ASCII_LETTERS and char not in ENGLISH_PUNCTUATION:
+            text = text.replace(char, ' ')
+
+    # shorten any extra dead space created above
+    text = text.replace('  ',' ')
 
     return text
 
@@ -38,9 +62,21 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
+    index = 0
+    while index + window_size < len(text):
+        inputs.append(text[index:index+window_size])
+        outputs.append(text[index+window_size])
+        index += step_size
+
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    # Layer 1, the LSTM module with 200 hidden units
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    # Layer 2, a fully-connected layer with softmax activation function
+    model.add(Dense(num_chars, activation='softmax'))
+    model.summary()
+    return model
